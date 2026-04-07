760b2dc0ab91382962c286ec47ad304571c5d510
Stanislovas Kujalis <stanislovas@pikta.lt>
2026-04-08 02:19:02 +0300

    flake update

--- gnocchi/flake.nix
+++ gnocchi/flake.nix
@@ -2,9 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixos-hardware }@inputs: {
