0ef91fe34e154f18ab901b9311b8c747cc8eb857
Stanislovas Kujalis <stanislovas@pikta.lt>
2026-05-14 22:36:05 +0300

    flake update

--- gnocchi/flake.nix
+++ gnocchi/flake.nix
@@ -2,24 +2,30 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }@inputs: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+    }@inputs:
+    {
 
-    nixosConfigurations.gnocchi = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      
-      # Pass flake inputs to modules (so you can use inputs.nixpkgs in other files)
-      specialArgs = { inherit inputs; };
-      
-      modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-t14-intel-gen6
-        ./configuration.nix
-        ./hardware-configuration.nix
-      ];
+      nixosConfigurations.gnocchi = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # Pass flake inputs to modules (so you can use inputs.nixpkgs in other files)
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t14-intel-gen6
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+
     };
-
-  };
 }
