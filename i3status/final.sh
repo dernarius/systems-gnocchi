2959fb3398f87eadc2b892c8b8eb7e95eda76788
Stanislovas Kujalis <stanislovas@pikta.lt>
2026-08-23 03:43:44 +0300

    new nvim

--- i3status/final.sh
+++ i3status/final.sh
@@ -3,6 +3,6 @@
 i3status | while :
 do
   read line
-  lang="$(swaymsg -rt get_inputs | jq -r '.[1].xkb_active_layout_name ')"
+  lang="$(swaymsg -rt get_inputs | jq -r '.[] | select(.type == "keyboard") | .xkb_active_layout_name ' | head -n 1)"
   echo "$lang $line" || exit 1
 done
